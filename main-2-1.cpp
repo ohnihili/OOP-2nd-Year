905856b55ddef81ec301a12e568755ec126d906b
ohnihili <104874178+ohnihili@users.noreply.github.com>
2023-03-09 13:37:15 +1030

    Add files via upload

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-extern int binary_to_int(int binary_digits[], int number_of_digits)
+extern int binary_to_int(int binary_digits[], int number_of_digits);
 
 using namespace std;
 
