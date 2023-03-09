2ceed0bc04a56353b2ad791dd0484c1c49700130
ohnihili <104874178+ohnihili@users.noreply.github.com>
2023-03-09 13:39:11 +1030

    Add files via upload

--- main-2-1.cpp
+++ main-2-1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 
-extern int binary_to_int(int binary_digits[], int number_of_digits);
+extern void print_binary_str(std::string decimal_number);
 
 using namespace std;
 
