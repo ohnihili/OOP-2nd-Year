654dc2dd4f9b116bf6a3ef1f635107908fe97ed0
ohnihili <104874178+ohnihili@users.noreply.github.com>
2023-04-27 10:31:02 +0930

    Add files via upload

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -8,14 +8,9 @@ int main()
     AirCraft plane(5000);
     plane.set_fuel(95.5);
     plane.set_numberOfFlights(5);
-
-    cout << "Aircraft weight is: " << plane.get_weight() << " Fuel: " << plane.get_fuel() << " Flights: " << plane.get_numberOfFlights() << endl;
-
     plane.refuel();
     plane.fly(2,1);
 
-    cout << "Fuel: " << plane.get_fuel() << endl;
-    cout << "Flights: " << plane.get_numberOfFlights() << endl;
 
 
 
